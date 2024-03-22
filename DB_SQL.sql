baaf27cea529c2af678594be2a6538f08eaaf845
Hamin Jung <java020103@gmail.com>
2024-03-22 14:50:48 +0900

    0322

---
+++ DB_SQL.sql
@@ -0,0 +1,22 @@
+/*
+CREATE DATABASE myfirstdb; DB만들기
+SHOW DATABASES;
+*/
+-- DROP DATABASE myfirstdb; DB지우기
+-- CREATE TABLE pet( 			/*테이블 이름 지어주기*/
+-- 	name VARCHAR(20),  		/*테이블 이름과 구성 다 이름을 지어줘야함*/
+-- 	owner VARCHAR(20),		/* CHAR - Character / VAR - variable(변수) */
+--     species VARCHAR(20),
+--     sex CHAR(1),
+--     birth date
+-- );
+
+-- INSERT INTO pet VALUES ('초롱이', '이은비', '고양이', 'F', '2013-02-14');
+-- INSERT INTO pet VALUES ('초코', '안현주', '고양이', 'M', '2014-03-17');
+-- INSERT INTO pet VALUES ('은별이', '조영섭', '고양이', 'F', '2009-05-13');
+-- INSERT INTO pet VALUES ('백구', '김종훈', '개', 'M', '2010-08-27');
+-- INSERT INTO pet VALUES ('웅산', '신상을', '개', 'M', '2009-08-31');
+-- INSERT INTO pet VALUES ('길산', '정하민', '개', 'F', '2008-09-11');
+
+SELECT * FROM pet;
+
